27994ea085139f6fcaca5a3c09c8e3eeafe9e09c
Rebecca Connie <rconnie@linux6.cs.uchicago.edu>
2022-03-12 01:35:37 -0600

    updated food_source_final to include gas station and liquor stores

--- chicagoeats/data_processing/clean_data.py
+++ chicagoeats/data_processing/clean_data.py
@@ -33,17 +33,25 @@ def create_year_df(filename):
     s15_16 = chi_food.loc[(chi_food['inspection_date'] >= '2015-01-01') & (chi_food['inspection_date'] < '2016-12-31')]
     s18_19 = chi_food.loc[(chi_food['inspection_date'] >= '2018-01-01') & (chi_food['inspection_date'] < '2019-12-31')]
 
-
     s12_13 = s12_13.drop_duplicates(['dba_name'], keep='first')
+    zero_license_13 = s12_13.loc[s12_13['license_'] == 0.0]
+    zero_df_13 = zero_license_13.iloc[1: ]
     df12_13 = s12_13.drop_duplicates(["license_"], keep='first')
-
+    df12_13_fin = pd.concat([zero_df_13, df12_13])
+    
     s15_16 = s15_16.drop_duplicates(['dba_name'], keep='first')
+    zero_license_16 = s15_16.loc[s15_16['license_'] == 0.0]
+    zero_df_16 = zero_license_16.iloc[1: ]
     df15_16= s15_16.drop_duplicates(["license_"], keep='first')
-
+    df15_16_fin = pd.concat([zero_df_16, df15_16])
+    
     s18_19 = s18_19.drop_duplicates(['dba_name'], keep='first')
+    zero_license_19 = s18_19.loc[s18_19['license_'] == 0.0]
+    zero_df_19 = zero_license_19.iloc[1: ]
     df18_19 = s18_19.drop_duplicates(["license_"], keep='first') 
+    df18_19_fin = pd.concat([zero_df_19, df18_19])
 
-    return (df12_13, df15_16, df18_19)
+    return (df12_13_fin, df15_16_fin, df18_19_fin)
 
 def create_facility_dfs(df, year):
     '''
@@ -57,15 +65,19 @@ def create_facility_dfs(df, year):
     df_restaurant_final = df_restaurant.reindex(columns = df_restaurant.columns.tolist() \
         + ['category'], fill_value = 'Restaurant')
 
-    df_convenience = df[df['facility_type'].str.contains('conv')]
+    df_convenience = df[df['facility_type'].str.contains('conv | gas')]
     df_convenience_final = df_convenience.reindex(columns = df_convenience.columns.tolist() \
-        + ['category'], fill_value = 'Convenience Store')
+        + ['category'], fill_value = 'Convenience Store/Gas Station')
 
     df_grocery = df[df['facility_type'].str.contains('groc')]
     df_grocery_final = df_grocery.reindex(columns = df_grocery.columns.tolist() + ['category'], \
         fill_value = 'Grocery Store') 
+
+    df_liquor = df[df['facility_type'].str.contains('liq')]
+    df_liquor_final = df_liquor.reindex(columns = df_liquor.columns.tolist() + ['category'], \
+        fill_value = 'Liquor Store') 
     
-    df_years_lst = [df_restaurant_final, df_convenience_final,  df_grocery_final]
+    df_years_lst = [df_restaurant_final, df_convenience_final,  df_grocery_final, df_liquor_final]
     
     return df_years_lst
 
@@ -76,6 +88,11 @@ def merge_df(df, year):
     df_years_lst = create_facility_dfs(df, year)
     final_duplicates = pd.concat(df_years_lst)
     final = final_duplicates.drop_duplicates(['dba_name'], keep='first')
+    zero_license_final = final.loc[final['license_'] == 0.0]
+    zero_df_final = zero_license_final.iloc[1: ]
     final_df = final.drop_duplicates(["license_"], keep='first') 
-   
-    return final_df
+    final_master_df = pd.concat([zero_df_final, final_df])
+
+    return final_master_df 
+
+
