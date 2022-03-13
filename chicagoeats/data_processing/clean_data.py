d8d0ab6a4d6a9eaf6dba1990dbd0b3701a53f18d
Rebecca Connie <rconnie@linux6.cs.uchicago.edu>
2022-03-13 01:54:03 -0600

    added create clean csv script updated food source csv

--- chicagoeats/data_processing/clean_data.py
+++ chicagoeats/data_processing/clean_data.py
@@ -2,29 +2,15 @@
 
 import pandas as pd
 
-def clean_main_data(filename):
+def create_year_df(FOOD_FILE):
     '''
     
     '''
-    (df_2013, df_2016, df_2019)= create_year_df(filename)
-
-    final_df13 = merge_df(df_2013, 2013)
-    final_df15 = merge_df(df_2016, 2016)
-    final_df19 = merge_df(df_2019, 2019)
-
-    final_df_lst = [final_df13, final_df15, final_df19]
-    final_food_df = pd.concat(final_df_lst)
-    
-    final_food_df.to_csv(r'~/capp30122/proj-chicago-eats/chicagoeats/data/food_source_final.csv')
-    
-
-def create_year_df(filename):
-    '''
-    
-    '''
-    chi_food = pd.read_csv(filename)
+    chi_food = pd.read_csv(FOOD_FILE)
     chi_food = chi_food.dropna()
     chi_food['license_'] = chi_food['license_'].fillna(0.0)
+    chi_food['dba_name'] = chi_food['dba_name'].str.lower()
+    chi_food['dba_name'] = chi_food['dba_name'].str.replace(r'[^\w\s]+', ' ', regex=True)
     chi_food['facility_type'] = chi_food['facility_type'].str.lower()
     chi_food['facility_type'] = chi_food['facility_type'].str.replace(r'[^\w\s]+', ' ', regex=True)
     chi_food['inspection_date'] = pd.to_datetime(chi_food['inspection_date'], format='%Y-%m-%d')
@@ -58,14 +44,15 @@ def create_facility_dfs(df, year):
     Input df
     Returns 1 df for the given year
     '''
-    
+
     df = df.reindex(columns = df.columns.tolist() + ['year'], fill_value = year)
 
-    df_restaurant = df[df['facility_type'].str.contains('rest')]
-    df_restaurant_final = df_restaurant.reindex(columns = df_restaurant.columns.tolist() \
-        + ['category'], fill_value = 'Restaurant')
-
-    df_convenience = df[df['facility_type'].str.contains('conv | gas')]
+    df_liquor = df[df['facility_type'].str.contains('liq') | df['dba_name'].str.contains('beverage')]
+    df_liquor_final = df_liquor.reindex(columns = df_liquor.columns.tolist() + ['category'], \
+        fill_value = 'Liquor Store') 
+    
+    df_convenience = df[df['facility_type'].str.contains('conv | gas') | \
+        df['dba_name'].str.contains('gas sta')]
     df_convenience_final = df_convenience.reindex(columns = df_convenience.columns.tolist() \
         + ['category'], fill_value = 'Convenience Store/Gas Station')
 
@@ -73,11 +60,11 @@ def create_facility_dfs(df, year):
     df_grocery_final = df_grocery.reindex(columns = df_grocery.columns.tolist() + ['category'], \
         fill_value = 'Grocery Store') 
 
-    df_liquor = df[df['facility_type'].str.contains('liq')]
-    df_liquor_final = df_liquor.reindex(columns = df_liquor.columns.tolist() + ['category'], \
-        fill_value = 'Liquor Store') 
-    
-    df_years_lst = [df_restaurant_final, df_convenience_final,  df_grocery_final, df_liquor_final]
+    df_restaurant = df[df['facility_type'].str.contains('rest')]
+    df_restaurant_final = df_restaurant.reindex(columns = df_restaurant.columns.tolist() \
+        + ['category'], fill_value = 'Restaurant')
+
+    df_years_lst = [df_liquor_final, df_convenience_final, df_grocery_final, df_restaurant_final]
     
     return df_years_lst
 
