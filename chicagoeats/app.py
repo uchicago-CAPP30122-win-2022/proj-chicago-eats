218b896190c0ad9490d98e2513dd2c7dac5f67a6
Andrew Warfield <andrew.warfield@protonmail.com>
2022-03-15 20:46:35 -0500

    add viz option to app.py - do some cleanup top

--- chicagoeats/app.py
+++ chicagoeats/app.py
@@ -2,28 +2,22 @@
 
 import argparse
 from data_processing.food_source_data_api import DataPortalCollector
+import os
 
 def main():
-    connector = DataPortalCollector()
-    limit = 1000000 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', help='the limit of the number of inspection records to receive')
-    #parser.add_argument('--des', action='store_true', help='Sort the libaries by name in Z to A order (default: ascending order)')
-  
+    parser.add_argument('--data', help='Downloads and cleans all ofthe data used in the project')
+    parser.add_argument('--viz', help='Launches the Panel server so that it can be viewed \
+    in a web browser.')
+
     args = parser.parse_args()
-    
-    if args.data: 
+
+    if args.data:
+        connector = DataPortalCollector()
         connector.find_records()
-
-    #if args.des:
-        #libraries.sort(reverse=True, key=lambda obj: obj.name)
-    #else:
-        #libraries.sort(key=lambda obj: obj.name)
-
-    #for library in libraries:
-        #print(library)
-        #print("----")
-
+    if args.viz:
+        cmd = "panel serve visualizations/viz.ipynb"
+        os.system(cmd)
 
 if __name__ == '__main__':
-    main() 
+    main()
