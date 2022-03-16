c6c0d232b03265650820d05efc5e463920174274
Andrew Warfield <andrew.warfield@protonmail.com>
2022-03-15 21:15:13 -0500

    add cenpy_fetch to app.py

--- chicagoeats/app.py
+++ chicagoeats/app.py
@@ -2,12 +2,13 @@
 
 import argparse
 from data_processing.food_source_data_api import DataPortalCollector
+from data_processing.cenpy_fetch import cenpy_fetcher
 import os
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', action='store_true',
-                        help='Downloads and cleans all ofthe data used in the project')
+                        help='Downloads and cleans all of the data used in the project')
     parser.add_argument('--viz', action='store_true',
                         help='Launches the Panel server so that it can be viewed in a web browser.')
 
@@ -16,6 +17,9 @@ def main():
     if args.data:
         connector = DataPortalCollector()
         connector.find_records()
+        print("Food data has been downloaded into the data folder.")
+        cenpy_fetcher()
+        print('cenpy data has been downloaded into the data folder.')
     if args.viz:
         cmd = "panel serve visualizations/viz.ipynb"
         os.system(cmd)
